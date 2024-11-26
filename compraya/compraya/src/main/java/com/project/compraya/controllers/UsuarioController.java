ddc6a516e71550460d2fe546a872cea2c57baace
andresperez2003 <andresap2017@gmail.com>
2024-11-26 17:54:15 -0500

    Backend terminado

--- compraya/compraya/src/main/java/com/project/compraya/controllers/UsuarioController.java
+++ compraya/compraya/src/main/java/com/project/compraya/controllers/UsuarioController.java
@@ -16,6 +16,9 @@ import com.project.compraya.entities.Usuario;
  * @author ASUS
  */
 import java.util.List;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/api/usuarios")
@@ -24,6 +27,23 @@ public class UsuarioController {
     @Autowired
     private UsuarioRepository usuarioRepository;
 
+        @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    // Endpoint para realizar login
+    @PostMapping("/login")
+    public String login(@RequestParam String email, @RequestParam String contrasenia) {
+        // Llama a la función almacenada login_usuario
+        String sql = "SELECT compraya.login_usuario(?, ?)";
+
+        // Ejecuta la función almacenada y obtiene el resultado
+        String resultado = jdbcTemplate.queryForObject(sql, new Object[]{email, contrasenia}, String.class);
+
+        // Devuelve el resultado obtenido de la función almacenada
+        return resultado;
+    }
+
+    
     @GetMapping("/get")
     public List<Usuario> listarUsuarios() {
         return usuarioRepository.findAll();
